3a6b115f9a0d62f6d6c148444a053a1e6cf753a2
Lara-diaz91 <lara.diaz@bitlogic.io>
2021-06-24 01:50:16 -0300

    Added store and functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import { Typography, Container } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import TodoList from './components/TodoList';
+import TodoAdd from './components/TodoAdd';
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+}));
 
 function App() {
+  const classes = useStyles();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container component="main" maxWidth="xs">
+        <div className={classes.paper}>
+          <Typography component="h1" variant="h5">
+            What's the Plan for Today?
+          </Typography>
+          <TodoAdd />
+          <TodoList />
+        </div>
+      </Container>
     </div>
   );
 }
---
+++ src/store.tsx
@@ -0,0 +1,38 @@
+import { makeAutoObservable } from 'mobx';
+
+interface Todo {
+  id: number;
+  text: string;
+}
+
+const removeTodo = (todos: Todo[], id: number): Todo[] =>
+  todos.filter((todo) => todo.id !== id);
+
+const addTodo = (todos: Todo[], text: string): Todo[] => [
+  ...todos,
+  {
+    id: Math.max(0, Math.max(...todos.map(({ id }) => id))) + 1,
+    text,
+  },
+];
+
+class Todos {
+  todos: Todo[] = [];
+  newTodo: string = '';
+
+  constructor() {
+    makeAutoObservable(this);
+  }
+
+  removeTodo(id: number) {
+    this.todos = removeTodo(this.todos, id);
+  }
+
+  addTodo() {
+    this.todos = addTodo(this.todos, this.newTodo);
+    this.newTodo = '';
+  }
+
+}
+
+export default new Todos()
